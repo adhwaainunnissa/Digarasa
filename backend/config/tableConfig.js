84464bd166fb61dd0fc860e9abc3f2165e80b5fa
zulfasowam <sowamzulfa@gmail.com>
2026-09-11 15:01:17 +0700

    nambahin data di ddatabase frontend

--- backend/config/tableConfig.js
+++ backend/config/tableConfig.js
@@ -4,6 +4,22 @@ const allowedTables = [
     "SKEMA_MT",
     "SKEMA_RELE",
     "Skema_RTAC",
+    "subsistem",
+    "OLS_STATIK",
+    "LIST_SW_OLS",
+    "His_HB_DS",
+    "His_Sw_OLS",
+    "RT_HB_DS",
+    "RT_Sw_OLS",
+    "RTAC_IOTEK",
+    "UFR_1",
+    "UFR_4",
+    "UFR_5",
+    "UFR_6",
+    "UFR_7",
+    "UP2D_UFR1",
+    "UP2D_UFR2",
+    "UP2D_UFR3",
 ];
 
 const isAllowedTable = (tableName) => {
